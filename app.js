773cfdf36513715e79303b26c7ffde0bd2bba090
Atul Shimpi <shimpiatul@hotmail.com>
2017-06-19 11:47:13 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var goalbar = angular.module('goalbar', []);
+
+goalbar.controller('GoalBarController', function($scope) {
+  $scope.current = 280;
+  $scope.goal = 500;
+  
+  $scope.infoAbove = false;
+
+  var calculateBarPercent = function() {
+    var percent = ($scope.current / $scope.goal) * 100;
+
+    if (percent > 100) {
+      percent = 100;
+    }
+    if (percent < 15) {
+      $scope.infoAbove = true;
+    }
+
+    return percent;
+  };
+  
+  $scope.$watch('current', function() {
+    $scope.barPercent = 100 - calculateBarPercent();
+    $scope.barStyle = 'transform: translateY(' + $scope.barPercent + '%)';
+  });
+});
