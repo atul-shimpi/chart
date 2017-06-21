41b41dd1c9ed20017dc8a897d19a9e56be5535c0
atul-shimpi <shimpiatul@hotmail.com>
2017-06-22 01:24:28 +0530

    Make scale dynamix

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 var goalbar = angular.module('goalbar', []);
 
 goalbar.controller('GoalBarController', function($scope) {
-  //$scope.current = 280;
-  $scope.values = [95.00, 97.50, 100.00, 102.50, 105, 107.5];
+  $scope.values = [95.00, 97.50, 100.00];
   $scope.goal = 500;
-  $scope.pos = 103.78;
+  $scope.pos = 96.5;
   
   $scope.infoAbove = false;
   
   var max = $scope.values[$scope.values.length - 1] - $scope.values[0];
   var achieved = $scope.pos - $scope.values[0];
   $scope.current = (achieved*500)/max;
-  
-  console.log("Max/Achieved/$scope.current : " + max + '/' + achieved + '/' + $scope.current);  
+   
   var calculateBarPercent = function() {    
     var percent = ($scope.current / $scope.goal) * 100;
     
-
+    percent = percent > 100 ? 100 : percent;
     return percent < 1 ? 0 : percent;
   };
   
   $scope.$watch('current', function() {
     $scope.barPercent = 100 - calculateBarPercent();
     $scope.barStyle = 'transform: translateY(' + $scope.barPercent + '%)';
-    console.log("Ssd" + $scope.barPercent);
   });
 });
