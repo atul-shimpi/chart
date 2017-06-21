ba4efe2f638153fdcc5de1f5240a2989dc1cfae6
Atul Shimpi <shimpiatul@hotmail.com>
2017-06-21 17:40:53 +0530

    Keep current dynamic

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 var goalbar = angular.module('goalbar', []);
 
 goalbar.controller('GoalBarController', function($scope) {
-  $scope.current = 300;
+  //$scope.current = 280;
+  $scope.values = [95.00, 97.50, 100.00, 102.50, 105, 107.5];
   $scope.goal = 500;
+  $scope.pos = 103.78;
   
   $scope.infoAbove = false;
-
-  var calculateBarPercent = function() {
+  
+  var max = $scope.values[$scope.values.length - 1] - $scope.values[0];
+  var achieved = $scope.pos - $scope.values[0];
+  $scope.current = (achieved*500)/max;
+  
+  console.log("Max/Achieved/$scope.current : " + max + '/' + achieved + '/' + $scope.current);  
+  var calculateBarPercent = function() {    
     var percent = ($scope.current / $scope.goal) * 100;
+    
 
-    if (percent > 100) {
-      percent = 100;
-    }
-    if (percent < 15) {
-      $scope.infoAbove = true;
-    }
-
-    return percent;
+    return percent < 1 ? 0 : percent;
   };
   
   $scope.$watch('current', function() {
     $scope.barPercent = 100 - calculateBarPercent();
     $scope.barStyle = 'transform: translateY(' + $scope.barPercent + '%)';
+    console.log("Ssd" + $scope.barPercent);
   });
 });
